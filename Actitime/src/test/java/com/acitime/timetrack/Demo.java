1250a2994d5c6a7b597dbb035e8be6c1dc6dd586
Asta7 <Asta7@Asta>
2020-09-01 12:53:12 +0530

    Commit Project

---
+++ Actitime/src/test/java/com/acitime/timetrack/Demo.java
@@ -0,0 +1,16 @@
+package com.acitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest()
+	
+	{
+		Reporter.log("Successfully running",true);	
+		Reporter.log("Successfully running",true);	Reporter.log("Successfully running",true);	
+	}
+	
+}
